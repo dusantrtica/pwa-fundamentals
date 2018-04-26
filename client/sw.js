3a5a7b9f04b049e097e91f197e661414cb624e95
Dusan <dusan.trtica@hotmail.com>
2018-04-26 18:07:29 +0200

    added sw

--- client/sw.js
+++ client/sw.js
@@ -1,17 +1,35 @@
-const counts = {
-	installs: 0,
-	activations: 0,
-	fetches: 0
-}
+const FALLBACK_IMAGE_URL = 'https://localhost:3100/images/fallback-grocery.png';
+
+const fallbackImages = 'fallback-images';
+
 
 self.addEventListener('install', (event) => {
-	console.log('instal', ++counts.installs)
+	event.waitUntil(
+		caches.open(fallbackImages)
+			.then(cache => {
+				cache.add(FALLBACK_IMAGE_URL);
+			})
+	)
 })
 
 self.addEventListener('activate', (event) => {
-	console.log('activate ', ++counts.activations)
+
 })
 
+function fetchImageOrFallback (fetchEvent) {
+	return fetch(fetchEvent.request, {
+		mode: 'no-cors'
+	}).then(({ response }) => {
+		if(!response.ok) {
+			return caches.match(FALLBACK_IMAGE_URL, {cacheName: fallbackImages})
+		} else {
+			return response
+		}
+	}).catch(() => {
+		return caches.match(FALLBACK_IMAGE_URL, {cacheName: fallbackImages})
+	})
+}
+
 self.addEventListener('fetch', (event) => {
-	console.log('fetch ', ++counts.fetches)
+
 })
